f61e206a1201aa3ab73e9bba786525fd5b542f0c
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2020-11-24 19:10:06 +0000

    added the functionality

--- spooky.js
+++ spooky.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //arrays
 eaten = ['Werewolf', 'Slime Monster', 'Ghost'];
 underBed = ['Demon', 'Boogey Man', 'Tentacle Creature'];
@@ -14,8 +13,4 @@ console.log(`There's a ${bed} under your bed!`);
 let doNot = doNotGo[Math.floor(Math.random() * doNotGo.length)];
 console.log(`Don't go ${doNot}!`);
 
-=======
-eaten = ['Werewolf', 'Slime Monster', 'Ghost'];
-underBed = ['Demon', 'Boogey Man', 'Tentacle Creature'];
-doNotGo = ['outside', 'under the stars', 'in there'];
->>>>>>> 4eceb5a12b8491e9731bbb9a6f247e2363e3e876
+
