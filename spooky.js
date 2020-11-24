64b8c80ac9b76f7f091e3c7e8992b8ff3834f90e
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2020-11-24 19:15:00 +0000

    functionality commit

--- spooky.js
+++ spooky.js
@@ -1,3 +1,15 @@
+//arrays
 eaten = ['Werewolf', 'Slime Monster', 'Ghost'];
 underBed = ['Demon', 'Boogey Man', 'Tentacle Creature'];
 doNotGo = ['outside', 'under the stars', 'in there'];
+
+//Log spooky messages
+let food = eaten[Math.floor(Math.random() * eaten.length)];
+console.log(`You will be eaten by a ${food} tonight!`);
+
+let bed = underBed[Math.floor(Math.random() * underBed.length)];
+console.log(`There's a ${bed} under your bed!`);
+
+let doNot = doNotGo[Math.floor(Math.random() * doNotGo.length)];
+console.log(`Don't go ${doNot}!`);
+
